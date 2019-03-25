dbd750656a340e1563e8a1e92b24a2fa5864ccea
JeffWhitfill <jeffwhitfill7@gmail.com>
2019-03-25 14:41:35 -0400

    Add files via upload

--- src/Components/Geocode.js
+++ src/Components/Geocode.js
@@ -33,24 +33,24 @@ class Geocode extends React.Component {
                 return response.json();
               }).then((response)=>{
                 this.setState({lat:response.results[0].geometry.lat,lng:response.results[0].geometry.lng},()=>{
-                  fetch(`https://api.sunrise-sunset.org/json?lat=${this.state.lat}&lng=${this.state.lng}&date=today`)
-                  .then(function (response) {
-                        return response.json();
-                      })
-                      .then((data)=>{
-                      this.setState({sunrise: data.results.sunrise});
-                      this.setState({sunset: data.results.sunset});
-                  }); 
+//                  fetch(`https://api.sunrise-sunset.org/json?lat=${this.state.lat}&lng=${this.state.lng}&date=today`)
+//                  .then(function (response) {
+//                        return response.json();
+//                      })
+//                      .then((data)=>{
+//                      this.setState({sunrise: data.results.sunrise});
+//                      this.setState({sunset: data.results.sunset});
+//                  }); 
                 })
-                fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=2208760fbb1e9cef062191db665bf8df`)
-                .then(function (response) {
-                    return response.json();
-                  })
-                  .then((response)=>{
-                  this.setState({temperature: response.main.temp});
-                  this.setState({humidity: response.main.humidity});
-                  this.setState({windSpeed: response.wind.speed})
-              });
+ //               fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=2208760fbb1e9cef062191db665bf8df`)
+ //               .then(function (response) {
+ //                   return response.json();
+ //                 })
+ //                 .then((response)=>{
+ //                 this.setState({temperature: response.main.temp});
+ //                 this.setState({humidity: response.main.humidity});
+ //                 this.setState({windSpeed: response.wind.speed})
+ //             });
               })
             
           
--- src/Components/Input.js
+++ src/Components/Input.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Input extends Component {
 
   state={
-    location: "Louisville"
+    location: ""
   }
     
 
