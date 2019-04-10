a3004954f593f0803d5fdc307d30f62173bdd408
JeffWhitfill <jeffwhitfill7@gmail.com>
2019-04-10 10:57:55 -0400

    Delete tester.js
    extra

--- src/Components/tester.js
+++
@@ -1,8 +0,0 @@
-//Get sunrise-sunset.org times
-fetch(`https://api.sunrise-sunset.org/json?lat=38.254238&lng=-85.759407&date=today`)
-    .then(function(response) {
-        return response.json();
-    })
-    .then (function(data) {
-        console.log(data);
-    });
