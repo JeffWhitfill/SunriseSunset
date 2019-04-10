236d4aab31992fbc24300a846957604d436f0a69
JeffWhitfill <jeffwhitfill7@gmail.com>
2019-04-10 11:18:39 -0400

    Add files via upload
    after clean up

---
+++ src/Components/Geocode.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Input from './Input'
+
+class Geocode extends React.Component {
+  // set initial state for latitude and longitude
+  state={
+    lat:"",
+    lng:""
+  }  
+  
+    // Receive location from input box
+    // Get coordinates from opencage API
+    onSubmitInput= (location)=> {        
+        fetch(`https://api.opencagedata.com/geocode/v1/json?q=${location}&key=40b6726e5f0045a9bcc9c3aca3b2261e`)
+            .then(function (response) {
+                return response.json();
+              })
+            .then((response)=>{
+              // Update state for latitude and longitude
+                this.setState({lat:response.results[0].geometry.lat,lng:response.results[0].geometry.lng},()=>{
+              }) 
+          })       
+  }
+
+  // Print latitude and longitude to the page
+
+    render() {
+        return (
+            <div className="container">
+              <h1 className="jumbotron text-center text-info">Geographic Coordinates</h1>
+                <Input onSubmitInput={this.onSubmitInput} />
+                <h3 className="bg-info text-white">Location Latitude</h3>
+                <p>Your latitude is {this.state.lat} .</p>
+                <h3 className="bg-info text-white">Location Longitude</h3>
+                <p>Your longitude is {this.state.lng} .</p>
+            </div>
+        )
+    }
+}
+
+export default Geocode;
---
+++ src/Components/SunriseSunset.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Input from './Input';
+
+class SunriseSunset extends React.Component {
+    // set initial state for sunrise and sunset
+    state={
+        sunrise:"",
+        sunset:"",
+    }
+
+    //Get sunrise-sunset.org times
+    // get latitude and longitude from geocode fetch
+    // use lat and lon to retreive sunset and sunrise data
+    onSubmitInput = (location) => {
+       fetch(`https://api.opencagedata.com/geocode/v1/json?q=${location}&key=40b6726e5f0045a9bcc9c3aca3b2261e`)
+            .then(function (response) {
+                return response.json();
+            }).then((response) => {
+                // update state for lat and long
+                this.setState({ lat: response.results[0].geometry.lat, lng: response.results[0].geometry.lng }, () => {
+                    fetch(`https://api.sunrise-sunset.org/json?lat=${this.state.lat}&lng=${this.state.lng}&date=today`)
+                        .then(function (response) {
+                            return response.json();
+                        })
+                        .then((data) => {
+                            // update state for sunrise and sunset
+                            this.setState({ sunrise: data.results.sunrise });
+                            this.setState({ sunset: data.results.sunset });
+                        });
+                })
+            })
+    }
+
+    // Print sunrise and sunset to screen
+
+    render() {
+        return (
+            <div className="container">
+                <h1 className="jumbotron text-center text-warning">Times for Sunrise and Sunset</h1>
+                <Input onSubmitInput={this.onSubmitInput} />
+                <h3 className="bg-warning">Location Sunrise</h3>
+                <p>Your Sunrise is projected for {this.state.sunrise} UTC</p>
+                <h3 className="bg-warning">Location Sunset</h3>
+                <p>Your Sunset is projected for {this.state.sunset} UTC </p>
+                <h6 className="font-italic">All times are in Coordinated Universal Time (UTC)</h6>
+            </div>
+        )
+    }
+
+}
+
+export default SunriseSunset;
